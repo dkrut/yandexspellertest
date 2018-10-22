a3bbc985c8921766494a385840643ee79dd4b6a9
master <dd.krutikov@gmail.com>
2018-10-23 01:09:19 +0300

    get assert to first test

--- src/test/java/TestSpellService.java
+++ src/test/java/TestSpellService.java
@@ -4,18 +4,24 @@ import net.yandex.speller.services.spellservice.CheckTextResponse;
 import net.yandex.speller.services.spellservice.SpellService;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.bind.Element;
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.IOException;
-import java.io.StringWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 /**
  * Created by Denis Krutikov on 21.10.2018.
@@ -30,49 +36,47 @@ public class TestSpellService {
 
 
     @Test
-    void testic() throws JAXBException, ParserConfigurationException, SAXException, IOException {
-        request.setText("слнyце");
+    void testTest() throws JAXBException, ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+        request.setText("малк");
         request.setLang("ru");
-        request.setFormat("html");
-        request.setOptions(1);
+//        request.setFormat("html");
+//        request.setOptions(1);
 
         response = spellService.getSpellServiceSoap().checkText(request);
 
+        File file = new File("src/test/resources/response.xml");
 
-        File file = new File("C:\\test\\qwe.xml");
+        jaxbWorker.convertObjectToXml(CheckTextResponse.class, response, file);
 
-
-        StringWriter writer = new StringWriter();
-        JAXBContext context = JAXBContext.newInstance(CheckTextResponse.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-
-        marshaller.marshal(response, file);
-
-        marshaller.marshal(response, writer);
-        String result = writer.toString();
-        System.out.println(result);
-
+//        StringWriter writer = new StringWriter();
+//
+//        JAXBContext context = JAXBContext.newInstance(CheckTextResponse.class);
+//        Marshaller marshaller = context.createMarshaller();
+//        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+//        marshaller.marshal(response, writer);
+//        String result = writer.toString();
+//        System.out.println(result);
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(file);
+        Document xmlDocument = builder.parse(file);
 
-        Element wrong = (Element) document.getElementsByTagName("s");
-//        String spellResult = wrong.getAttribute("SpellResult");
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String expression = "//s";
 
+//Выбрать только первый элемент
+//        String s = xPath.compile(expression).evaluate(xmlDocument);
+//        System.out.println(s);
 
+//Выбрать все элементы
+        NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+        List<String> list = new LinkedList<String>();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            list.add(nodeList.item(i).getFirstChild().getNodeValue());
+        }
+
+        assertThat(list, hasItems("Марк", "мал"));
 
     }
-
-    @Test
-    void asd(){
-        File file = new File("C:\\test\\qwe.xml");
-        request.setText("кaлас");
-        response = spellService.getSpellServiceSoap().checkText(request);
-
-        jaxbWorker.convertObjectToXml(CheckTextResponse.class, response, file);
-    }
-
-
 }
